128a2842a9ed06c5ed8a6fabf0b72210b6afc41e
Papon <tattoo197@outlook.com>
2021-03-21 13:47:32 +0700

    add image, model struct data

---
+++ SwiftUiPractice/Model/LandMark.swift
@@ -0,0 +1,15 @@
+//
+//  LandMark.swift
+//  SwiftUiPractice
+//
+//  Created by Papon Supamongkonchai on 21/3/2564 BE.
+//
+import Foundation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+}
