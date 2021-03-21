2546bae1809afdb506ef87ee1735d259d02765dd
Papon <tattoo197@outlook.com>
2021-03-21 16:26:37 +0700

    Create a Favorite Button for Each Landmark

---
+++ SwiftUiPractice/LandMark/components_landmark/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  SwiftUiPractice
+//
+//  Created by Papon Supamongkonchai on 21/3/2564 BE.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? Color.yellow : Color.gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
