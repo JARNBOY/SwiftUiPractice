66dd18395b1b946e6155ad7d4a9a11a06f7075a7
papon@wecomics.in.th <tattoo197@outlook.com>
2021-03-21 12:11:22 +0700

    Initial Commit

---
+++ SwiftUiPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiPractice
+//
+//  Created by Papon Supamongkonchai on 21/3/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
