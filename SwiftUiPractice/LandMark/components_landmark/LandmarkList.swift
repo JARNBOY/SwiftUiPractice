ce709902d68e543cf9a0cbc923af3042d722c411
Papon <tattoo197@outlook.com>
2021-03-21 15:08:53 +0700

    "iPad Pro (12.9-inch) (2nd generation)"

--- SwiftUiPractice/LandMark/components_landmark/LandmarkList.swift
+++ SwiftUiPractice/LandMark/components_landmark/LandmarkList.swift
@@ -27,7 +27,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max","iPad Pro (12.9-inch) (2nd generation)"], id: \.self) { deviceName in
                     LandmarkList()
                         .previewDevice(PreviewDevice(rawValue: deviceName))
                         .previewDisplayName(deviceName)
