a1f087682d75672cf41aadc51f7b870ae88c94f3
Papon <tattoo197@outlook.com>
2021-03-21 14:24:20 +0700

    Create the List of Landmarks

---
+++ SwiftUiPractice/LandMark/components_landmark/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  SwiftUiPractice
+//
+//  Created by Papon Supamongkonchai on 21/3/2564 BE.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
